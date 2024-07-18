66c6a84a37a306d2ee24389d3258ae498e0ffbff
YanFlandre <jp_felippe@hotmail.com>
2024-07-18 13:55:39 -0300

    Fixing errors

--- index.ts
+++ index.ts
@@ -15,10 +15,14 @@ axios.get(url).then(Response => {
   const title =todo.title;
   const completed = todo.completed;
 
-  console.log(`
-     The Todo with ID: ${ID}
-     Has a title of: ${title}
-     Is it finished? ${completed}
-    `);
+  logTodo(ID, title, completed);
 
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+   `);
+}
