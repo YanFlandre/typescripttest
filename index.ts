e6bc11dbd7a85374f85128305d4032cc40047e44
YanFlandre <jp_felippe@hotmail.com>
2024-07-18 13:50:41 -0300

    Adding TS annotation todo

--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
 import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo{
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(Response => {
-  console.log(Response.data);
+  const todo = Response.data as Todo;
+
+  const ID=todo.id;
+  const title =todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+     The Todo with ID: ${ID}
+     Has a title of: ${title}
+     Is it finished? ${completed}
+    `);
 
 });
